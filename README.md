ec62f4eb2855dd4fc68ab47274150218edf549d5
Ståle Gjelsten <stalegjelsten@gmail.com>
2020-07-29 20:57:35 +0200

    Added theory with equations and updated the readme file. Still not finished.

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Solar system animation
 
-TODO
+This repo contains a python script to simulate solar systems. The script has only been tested with our own [Solar System](https://en.wikipedia.org/wiki/Solar_System).
+
+## Requirements
+
+The script requires the following modules:
+
+- datetime
+- spiceypy
+- scipy
+- numpy
+- matplotlib
+
+## How the script works
+
+Enter the simulation start and end dates in the start_date_utc variables. The sec_per_iteration variable controls the precision of the calculations and the speed of the animation. The default value of 86 400 is equal to one day (60 sec/min \* 60 min/hour \* 24 hours/day), which means we perform one calculation of the orbits per day and that every frame of the animation represents one day.
+
+Initialize a planetary system by doing something like `Sun = planetary_system("Sun", sun_mass_in_kg)`.
+
+Initialize planets as `Planet` instances by doing something like `earth = Planet(399, "Earth", Sun)`, where Sun is the planetary_system instance you constructed earlier. 399 is the [NASA NAIF ID](https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/FORTRAN/req/naif_ids.html) for the planet.
+
+TODO: write more text here... 😅
--- sunearth-oop.py
+++ sunearth-oop.py
@@ -1,4 +1,49 @@
-# object oriented sun earth simulation
+#%% [markdown]
+# # Solar system simulator
+# 
+# By Ståle Gjelsten.
+#
+# This script was written in the summer of 2020 to improve on a program I had written for
+# [pico-8](https://www.lexaloffle.com/pico-8.php). Because of the pico-8's limited float and
+# integer sizes (and the fact that I had to use [Euler method](https://en.wikipedia.org/wiki/Euler_method) 
+# for integration), the orbit calculations were way off. This script is by no means an accurate representation
+# of a planetary system -- it only calculates forces between each satelite and the star -- but at least
+# it seems to be fairly close to other simulations and measurements.
+#
+# ## Theory on satelite orbits
+# 
+# The gravitational force $\vec{F}$ on a satelite with mass $m$ is given by:
+# $$ {F}_G = - \frac{GMm}{r^2} $$
+# Where $G$ is the gravitational constant, $M$ is the mass of the central object (the Sun in our case), 
+# $m$ is the mass of the satelite and $r^2$ is the square of the distance between the objects.
+# 
+# We can also express the gravitational force as a vector:
+# $$ \vec{F}_G = - \frac{GMm}{r^3}\vec{r} $$
+# where $\vec{r}$ is the position vector and $r=\sqrt{ \lvert x \rvert}$.
+#
+# From Newtons second law we have $\sum{\vec{F}} = m\vec{a}$ and substituting for $\vec{F}_G$ gives:
+# $$ m\vec{a} = - \frac{GMm}{r^3}\vec{x} \Leftrightarrow \vec{a} = - \frac{GM}{r^3}\vec{x} $$
+#
+# We can find the velocity and position of the satelite by integration as the acceleration of the satelite
+# is the derivative of the velocity and the double derivative of the position.
+# $$ \vec{\ddot r} = \vec{\dot v} = \vec{a} $$
+#
+# In this script we find the positions by numerical integration. To accomplish that we construct a vector $\vec{x}(t)$ such that:
+# $$ \vec{x}(t) = \begin{bmatrix} x(t)\\ y(t) \\ z(t) \\ v_x(t) \\ v_y(t) \\ v_z(t) \end{bmatrix} $$
+# This state vector contains both the position and velocity of the satelite.
+# We also define the function $f$ with the derivative of $\vec{x}(t)$ and use numerical integration to find the solution
+# to our differential equation.
+# $$ f(\vec{x},t) = \begin{bmatrix} v_x(t)\\ v_y(t)\\ v_z(t)\\ F_x/m\\ F_y/m\\ F_z/m \end{bmatrix} $$
+#
+# The initial values for our initial value problem are sourced from [NASAs SPICE Toolkit](https://naif.jpl.nasa.gov/naif/toolkit.html).
+#
+# Suggested reading on python simulation of the two-body problem:
+#
+# - [Scientific computing toolbox's introduction](https://faculty1.coloradocollege.edu/~sburns/toolbox/ODE_II.html) to 
+# `odeint()` in python
+# - [C.P. Dullemond of the University of Heidelberg's introduction to the N-body model](http://www.ita.uni-heidelberg.de/~dullemond/lectures/studtage_compastro_2018/Chapter_1.pdf)
+# - [University of Oslo AST1100 lecture notes on the general solution to the two-body problem](https://www.uio.no/studier/emner/matnat/astro/AST1100/h13/undervisningsmateriale/ast1100-fullstendig.pdf)
+
 #%%
 import numpy as np
 import spiceypy
@@ -6,27 +51,29 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import datetime
 import matplotlib.animation as animation
-from IPython.display import HTML
+# from IPython.display import HTML
 
 # loading with kernels with planetary data for NASAs SPICE module
-spiceypy.furnsh("../_kernels/pck/gm_de431.tpc")
-spiceypy.furnsh("../_kernels/lsk/naif0012.tls")
-spiceypy.furnsh("../_kernels/spk/de432s.bsp")
-spiceypy.furnsh("../_kernels/pck/pck00010.tpc")
+kernel_dir = "../_kernels/"        # directory with SPICE kernels
 
-# "universal" constants
+spiceypy.furnsh(kernel_dir + "pck/gm_de431.tpc")
+spiceypy.furnsh(kernel_dir + "lsk/naif0012.tls")
+spiceypy.furnsh(kernel_dir + "spk/de432s.bsp")
+spiceypy.furnsh(kernel_dir + "pck/pck00010.tpc")
+
+# time variables
 start_date_utc = datetime.datetime(year=2000, month=1, day=1)
-end_date_utc = datetime.datetime(year=2000, month=9, day=1)
-start_date_et = spiceypy.utc2et(start_date_utc.strftime("%Y-%m-%dT00:00:00"))
-end_date_et = spiceypy.utc2et(end_date_utc.strftime("%Y-%m-%dT00:00:00"))
-
-year = 365*86400                            # sec in year [s]
-au = 1.49598e11                             # AU in m     [m]
+end_date_utc = datetime.datetime(year=2001, month=9, day=1)
 sec_per_iteration = 86400                   # seconds per iteration of the
                                             # calculations and animation
 
+# "universal" constants
+year = 365*86400                            # sec in year [s]
+au = 1.49598e11                             # AU in m     [m]
 
 
+start_date_et = spiceypy.utc2et(start_date_utc.strftime("%Y-%m-%dT00:00:00"))
+end_date_et = spiceypy.utc2et(end_date_utc.strftime("%Y-%m-%dT00:00:00"))
 duration = (end_date_utc - start_date_utc).days * 86400  # sim. duration in [s]
 nt = int(np.round(duration / sec_per_iteration, 0))      # number of steps
 t = np.linspace(0, duration, nt)                         # evenly spaced time
@@ -42,7 +89,7 @@ class Planet:
 
     def __init__(self, naifid, name, orbiting):
         # planets are constructed with their NASA NAIF ID's (int), name (str)
-        # and the body they are orbiting (solar_system instance) 
+        # and the body they are orbiting (planetary_system instance) 
 
         self.name = name
         self.naifid = naifid
@@ -60,6 +107,7 @@ class Planet:
         self.vis_area = 2 * self.radii ** 2 * np.pi
         self.state = self.state*1000                        # km -> m conv.
         self.parent = orbiting
+        self.gm = orbiting.G*orbiting.sun_mass
         self.y = []
         self.line = []
         self.anim_data = []
@@ -67,27 +115,26 @@ class Planet:
         self.color = []
         orbiting.add_planet(self)
     
-    def f(self, y, t, mstar):
-        self.gm = solar_system.G*mstar
-        self.x = y[0:3]
-        self.v = y[3:]
-        self.r = np.linalg.norm(self.x)
-        self.dxdt = self.v
-        self.dvdt = -self.gm*self.x/self.r**3
-        self.dy = np.hstack((self.dxdt, self.dvdt))
+    def f(self, y, t):
+        x = y[0:3]
+        v = y[3:]
+        r = np.linalg.norm(x)
+        dxdt = v
+        dvdt = -self.gm*x/r**3
+        self.dy = np.hstack((dxdt, dvdt))
         return self.dy
     
     def plot_orbit_2d(self):
-        y = self.calculate_orbit()                      
-        self.y = y
-        self.line, = self.parent.ax.plot(y[:,0],y[:,1], color=self.color)
+        self.y = self.calculate_orbit()                      
+        self.line, = self.parent.ax.plot(self.y[:,0],self.y[:,1], \
+                                         color=self.color)
         self.line.set_label(self.name)
         legend_objects.append(self.line)
         legend_titles.append(self.name)
         return self.line, self.y
 
     def calculate_orbit(self):
-        y = odeint(self.f, self.state,t, args=(self.parent.sun_mass,))
+        y = odeint(self.f, self.state,t)
         y = y/au                                        # divide by AU 
         return y
     
@@ -110,7 +157,7 @@ class Planet:
         return cls(id, name, orbiting)
 
     
-class solar_system:
+class planetary_system:
 
     G = 6.67428e-11                             # m3 / kg / s^2
     def __init__(self, name, sun_mass):
@@ -172,10 +219,11 @@ class solar_system:
             #     scat.set_array(self.planets[j].color)
             time = start_date_utc + datetime.timedelta(days=i)
             time_str = time.strftime("%Y-%m-%d")
-            title.set_text("Date: " + time_str)
-            return scat,title
+            timer.set_text("Date: " + time_str)
+            return scat,timer
 
-        title = self.ax.text(1,1.03, "", ha="right", va="top", transform=self.ax.transAxes)
+        timer = self.ax.text(1,1.03, "", ha="right", va="top", \
+                             transform=self.ax.transAxes)
 
 
         self.scat = self.ax.scatter([], [])
@@ -189,7 +237,7 @@ class solar_system:
 
 # Let's add our solar system as an instance
 
-Sun = solar_system("Sun", 1.98892e30)
+Sun = planetary_system("Sun", 1.98892e30)
 
 # create our solar system and save Axes object as space_map
 Sun.ax, Sun.fig = Sun.create_space_map()
